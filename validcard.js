2d96a3857a4a70186565635d19ee964957402a1d
Shellie Garibo Munoz <ayame-knight@Shellies-MacBook-Pro.local>
2023-03-03 20:30:01 -0800

    Update validateCardNumber function to hide card information with the last 4 digit revealed

--- validcard.js
+++ validcard.js
@@ -1,9 +1,18 @@
 const validateCardNumber = (number) => {
-    let regexp = /(^[0-9]{12,16})$/;
-    if (regexp.test(number)) {
-        return true;
+    regexp = /(^[0-9]{12,16})$/;
+        if (regexp.test(number)) {
+            let hideNum = [];
+                for(let i = 0; i < number.length; i++){
+                    if(i < number.length-4){
+                        hideNum.push("*");
+                    }else{
+                        hideNum.push(number[i]);
+                    }
+                }
+        return hideNum.join("");
+        
     } else {
-        return "Enter a valid Credit card";
+        return "Invalid Credit card";
     }
 }
 
